0919f00ceb2a1a1b2da7c3956c451a43f40c4378
Serg Prog <progserg.85@mail.ru>
2016-11-26 19:17:51 +0300

    закончено меню с большим привью, боковое меню. подготовка сетки для мелкого просмотра товаров

--- js/script.js
+++ js/script.js
@@ -1,47 +1,12 @@
 ;
 
-function backgroundNewFirstOffer() {
-    var elem = document.getElementById('offer-link-1');
-    elem.style.background = 'url("../img/offer-list-background.png")';
-    elem.querySelector('div a').style.color = '#2f3638';
-    //elem.setAttribute('style', document.styleSheets[0].cssRules['.offers-menu ul li#offer-link-2:hover']);
-
-}
-function backgroundNewAnotherOffer(e) {
-    var elem_id = e.id;
-    var elem = document.getElementById(e.id);
-    //alert(elem);
-    switch (elem_id){
-        case 'offer-link-2':
-            elem.style.background = 'url("../img/offer-list-background-hover-2.png")';
-            elem.querySelector('div a').style.color = '#ffffff';
-            break;
-        case 'offer-link-3':
-            elem.style.background = 'url("../img/offer-list-background-hover-3.png")';
-            elem.querySelector('div a').style.color = '#ffffff';
-            break;
-        case 'offer-link-4':
-            elem.style.background = 'url("../img/offer-list-background-hover-4.png")';
-            elem.querySelector('div a').style.color = '#ffffff';
-            break;
-        case 'offer-link-5':
-            elem.style.background = 'url("../img/offer-list-background-hover-5.png")';
-            elem.querySelector('div a').style.color = '#ffffff';
-            break;
-    }
-    //elem.querySelector('div a').style.color = '#ffffff';
-}
-function backgroundDefaultFirstOffer() {
-    var elem = document.getElementById('offer-link-1');
-    elem.style.background = 'url("../img/offer-list-background-hover-1.png")';
-    elem.querySelector('div a').style.color = '#ffffff';
-}
 function changeAll(e) {
     var elem_id = e.id;
     var elem = document.getElementById(e.id);
     var elems = document.querySelectorAll('div.offers-menu > ul > li');
     var img ='';
-    var i=0;
+    var text='';
+    var i = 0;
     while (elems[i]!=null){
         if(elems[i].id != elem_id){
             elem = document.getElementById(elems[i].id);
@@ -53,32 +18,62 @@ function changeAll(e) {
             switch (elem_id){
                 case 'offer-link-1':
                     elem.style.background = 'url("../img/offer-list-background-hover-1.png")';
+                    text = document.getElementById('offer-text-1');
+                    text.style.display = 'block';
+                    document.getElementById('offer-text-2').style.display = 'none';
+                    document.getElementById('offer-text-3').style.display = 'none';
+                    document.getElementById('offer-text-4').style.display = 'none';
+                    document.getElementById('offer-text-5').style.display = 'none';
                     img = document.querySelector('div.offer-info');
-                    img.setAttribute('div.offer-info', document.styleSheets[0].cssRules['.offer-info:hover'])
+                    img.setAttribute('style', document.styleSheets[0].cssRules['.offer-info:hover'])
                     img.style.backgroundImage = 'url("../img/selected-offer-1.png';
                     break;
                 case 'offer-link-2':
                     elem.style.background = 'url("../img/offer-list-background-hover-2.png")';
+                    text = document.getElementById('offer-text-2');
+                    text.style.display = 'block';
+                    document.getElementById('offer-text-1').style.display = 'none';
+                    document.getElementById('offer-text-3').style.display = 'none';
+                    document.getElementById('offer-text-4').style.display = 'none';
+                    document.getElementById('offer-text-5').style.display = 'none';
                     img = document.querySelector('div.offer-info');
-                    img.setAttribute('div.offer-info', document.styleSheets[0].cssRules['.offer-info:hover'])
+                    img.setAttribute('style', document.styleSheets[0].cssRules['.offer-info:hover'])
                     img.style.backgroundImage = 'url("../img/selected-offer-2.png';
                     break;
                 case 'offer-link-3':
                     elem.style.background = 'url("../img/offer-list-background-hover-3.png")';
+                    text = document.getElementById('offer-text-3');
+                    text.style.display = 'block';
+                    document.getElementById('offer-text-1').style.display = 'none';
+                    document.getElementById('offer-text-2').style.display = 'none';
+                    document.getElementById('offer-text-4').style.display = 'none';
+                    document.getElementById('offer-text-5').style.display = 'none';
                     img = document.querySelector('div.offer-info');
-                    img.setAttribute('div.offer-info', document.styleSheets[0].cssRules['.offer-info:hover'])
+                    img.setAttribute('style', document.styleSheets[0].cssRules['.offer-info:hover'])
                     img.style.backgroundImage = 'url("../img/selected-offer-3.png';
                     break;
                 case 'offer-link-4':
                     elem.style.background = 'url("../img/offer-list-background-hover-4.png")';
+                    text = document.getElementById('offer-text-4');
+                    text.style.display = 'block';
+                    document.getElementById('offer-text-1').style.display = 'none';
+                    document.getElementById('offer-text-2').style.display = 'none';
+                    document.getElementById('offer-text-3').style.display = 'none';
+                    document.getElementById('offer-text-5').style.display = 'none';
                     img = document.querySelector('div.offer-info');
-                    img.setAttribute('div.offer-info', document.styleSheets[0].cssRules['.offer-info:hover'])
+                    img.setAttribute('style', document.styleSheets[0].cssRules['.offer-info:hover'])
                     img.style.backgroundImage = 'url("../img/selected-offer-4.png';
                     break;
                 case 'offer-link-5':
                     elem.style.background = 'url("../img/offer-list-background-hover-5.png")';
+                    text = document.getElementById('offer-text-5');
+                    text.style.display = 'block';
+                    document.getElementById('offer-text-1').style.display = 'none';
+                    document.getElementById('offer-text-2').style.display = 'none';
+                    document.getElementById('offer-text-3').style.display = 'none';
+                    document.getElementById('offer-text-4').style.display = 'none';
                     img = document.querySelector('div.offer-info');
-                    img.setAttribute('div.offer-info', document.styleSheets[0].cssRules['.offer-info:hover'])
+                    img.setAttribute('style', document.styleSheets[0].cssRules['.offer-info:hover'])
                     img.style.backgroundImage = 'url("../img/selected-offer-5.png';
                     break;
             }
@@ -87,29 +82,4 @@ function changeAll(e) {
         i++;
     }
 
-    /*switch (elem_id){
-        case 'offer-link-1':
-            elem.style.background = 'url("../img/offer-list-background-hover-1.png")';
-            elem.querySelector('div a').style.color = '#ffffff';
-            while {
-
-            }
-            break;
-        case 'offer-link-2':
-            elem.style.background = 'url("../img/offer-list-background-hover-2.png")';
-            elem.querySelector('div a').style.color = '#ffffff';
-            break;
-        case 'offer-link-3':
-            elem.style.background = 'url("../img/offer-list-background-hover-3.png")';
-            elem.querySelector('div a').style.color = '#ffffff';
-            break;
-        case 'offer-link-4':
-            elem.style.background = 'url("../img/offer-list-background-hover-4.png")';
-            elem.querySelector('div a').style.color = '#ffffff';
-            break;
-        case 'offer-link-5':
-            elem.style.background = 'url("../img/offer-list-background-hover-5.png")';
-            elem.querySelector('div a').style.color = '#ffffff';
-            break;
-    }*/
 }
